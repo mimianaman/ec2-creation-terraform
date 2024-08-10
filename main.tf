f3757e8ef55e1f17cd56d54705134144b46708f6
mimi <54443404+mimianaman@users.noreply.github.com>
2024-08-10 14:14:56 +0000

    update
    replaced the old ami with a new one from the aws console

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "this" {
   count                   = "5"
-  ami                     = "ami-0dcc1e21636832c5d"
+  ami                     = "ami-0ae8f15ae66fe8cda"
   instance_type           = "t2.micro"
   subnet_id                = "subnet-0e694b558e05b6d2f"
   key_name                 = "terraform-zero-hero"
